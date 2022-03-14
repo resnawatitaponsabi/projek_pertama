76e4e2f4eeccd837cf5af3c028bd862ae942c9ad
resnawatitaponsabi <81347085+resnawatitaponsabi@users.noreply.github.com>
2022-03-14 15:24:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ include 'functions.php';
 
 <div class="content">
 	<h2>Home</h2>
-	<p>Welcome to the home page! echa</p>
+	<p>Welcome to the home page! echa  lagi apa</p>
 </div>
 
 <?=template_footer()?>
